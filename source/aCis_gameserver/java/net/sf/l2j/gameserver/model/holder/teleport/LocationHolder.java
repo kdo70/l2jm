99cb2b8dc9818062789f41b00b1803b65a722130
Кривошеев Дмитрий <d.krivosheev70@gmail.com>
2022-12-13 23:51:24 +0700

    add java doc

--- source/aCis_gameserver/java/net/sf/l2j/gameserver/model/holder/teleport/LocationHolder.java
+++ source/aCis_gameserver/java/net/sf/l2j/gameserver/model/holder/teleport/LocationHolder.java
@@ -2,18 +2,32 @@ package net.sf.l2j.gameserver.model.holder.teleport;
 
 import net.sf.l2j.commons.data.StatSet;
 
+/**
+ * Holder: location gatekeeper
+ */
 public class LocationHolder {
+
     private final String _name;
     private final String _capital;
 
+    /**
+     * @param set stat set
+     */
     public LocationHolder(StatSet set) {
         _name = set.getString("name");
         _capital = set.getString("capital");
     }
 
+    /**
+     * @return name
+     */
     public final String getName() {
         return _name;
     }
+
+    /**
+     * @return capital
+     */
     public final String getCapital() {
         return _capital;
     }
--- source/aCis_gameserver/java/net/sf/l2j/gameserver/model/holder/teleport/TeleportHolder.java
+++ source/aCis_gameserver/java/net/sf/l2j/gameserver/model/holder/teleport/TeleportHolder.java
@@ -3,18 +3,35 @@ package net.sf.l2j.gameserver.model.holder.teleport;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Holder: teleport gatekeeper
+ */
 public class TeleportHolder {
+
     private final int _id;
+
+    /**
+     * Locations
+     */
     protected List<LocationHolder> _locations = new ArrayList<>();
 
+    /**
+     * @param id id
+     */
     public TeleportHolder(int id) {
         _id = id;
     }
 
+    /**
+     * @return id
+     */
     public final int getId() {
         return _id;
     }
 
+    /**
+     * @return locations
+     */
     public List<LocationHolder> getLocations() {
         return _locations;
     }
